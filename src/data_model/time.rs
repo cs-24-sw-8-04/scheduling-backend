4453fc95260fefbfe71b821364280d35590bcc1b
Mads Mogensen <mail@madsmogensen.dk>
2024-03-26 14:44:51 +0100

    Implement REST api that can create and get tasks saved in the database
    Axum is used for the web framework and sqlx is used for connecting to
    the database.
    The database is a sqlite database for simplicity.
    
    The folder migrations contains sql commands that setup a sql database
    from scratch.
    It is also used to upgrade an older sql database schema to a newer one.
    Sqlx keeps track of the schema version the database uses.
    
    To build you need some one-time setup:
    First create a file named .env in the root of the project.
    This contains the connection string used for connecting to the database.
    It can look like this:
    ```
    DATABASE_URL=sqlite://dev.db
    ```
    Then you need to install the sqlx commandline tool.
    Use it to create the database and run the available migrations.
    ```
    cargo install sqlx-cli
    cargo sqlx db create
    cargo sqlx migrate run
    ```

---
+++ src/data_model/time.rs
@@ -0,0 +1,49 @@
+use chrono::{DateTime, Duration, NaiveDateTime, TimeZone, Utc};
+use serde::{Deserialize, Serialize};
+
+pub type DateTimeUtc = DateTime<Utc>;
+
+#[derive(Serialize, Deserialize, sqlx::Type)]
+#[sqlx(transparent)]
+pub struct Milliseconds(i64);
+
+impl Into<Duration> for Milliseconds {
+    fn into(self) -> Duration {
+        Duration::try_milliseconds(self.0).unwrap()
+    }
+}
+
+impl From<Duration> for Milliseconds {
+    fn from(value: Duration) -> Self {
+        Milliseconds(value.num_milliseconds())
+    }
+}
+
+impl From<i64> for Milliseconds {
+    fn from(value: i64) -> Self {
+        Milliseconds(value)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Timespan {
+    pub start: DateTimeUtc,
+    pub end: DateTimeUtc,
+}
+
+impl Timespan {
+    pub fn new(start: DateTimeUtc, end: DateTimeUtc) -> Self {
+        Timespan { start, end }
+    }
+
+    pub fn new_from_naive(start: NaiveDateTime, end: NaiveDateTime) -> Self {
+        fn to_utc(date_time: NaiveDateTime) -> DateTimeUtc {
+            Utc::from_utc_datetime(&Utc, &date_time)
+        }
+
+        Timespan {
+            start: to_utc(start),
+            end: to_utc(end),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,102 @@
-mod data_model;
-use data_model::task::hello;
+use axum::{
+    debug_handler,
+    extract::State,
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
+use std::error::Error;
+use tokio::net::TcpListener;
 
-fn main() {
-    hello();
+use dotenv::dotenv;
+
+mod data_model;
+
+use data_model::task::Task;
+use data_model::time::Timespan;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv()?;
+
+    let db_connection_string = std::env::var("DATABASE_URL")?;
+
+    let pool = SqlitePoolOptions::new()
+        .connect(&db_connection_string)
+        .await?;
+
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    let app = Router::new()
+        .route("/", get(get_tasks))
+        .route("/create", post(create_task))
+        .with_state(pool);
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+#[debug_handler]
+async fn get_tasks(
+    State(pool): State<SqlitePool>,
+) -> Result<Json<Vec<Task>>, (StatusCode, String)> {
+    let tasks = sqlx::query!(
+        r#"
+        SELECT id, timespan_start, timespan_end, duration, effect
+    FROM Tasks
+    "#
+    )
+    .fetch_all(&pool)
+    .await
+    .map_err(internal_error)?;
+
+    let my_tasks = tasks
+        .iter()
+        .map(|t| Task {
+            id: t.id,
+            timespan: Timespan::new_from_naive(t.timespan_start, t.timespan_end),
+            duration: t.duration.into(),
+            effect: t.effect,
+        })
+        .collect();
+
+    Ok(Json(my_tasks))
+}
+
+#[debug_handler]
+async fn create_task(
+    State(pool): State<SqlitePool>,
+    Json(mut task): Json<Task>,
+) -> Result<Json<Task>, (StatusCode, String)> {
+    let id = sqlx::query_scalar!(
+        r#"
+    INSERT INTO Tasks (timespan_start, timespan_end, duration, effect)
+    VALUES (?, ?, ?, ?)
+    RETURNING id
+    "#,
+        task.timespan.start,
+        task.timespan.end,
+        task.duration,
+        task.effect
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(internal_error)?;
+
+    task.id = id;
+
+    Ok(Json(task))
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
